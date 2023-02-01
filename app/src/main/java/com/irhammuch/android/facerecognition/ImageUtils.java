d71bd112aa21033f56bcba8abf9575f5b15b3c2e
Oscaner Miao <oscaner1997@gmail.com>
2023-02-01 18:05:55 +0800

    image.

---
+++ app/src/main/java/com/irhammuch/android/facerecognition/ImageUtils.java
@@ -0,0 +1,200 @@
+package com.irhammuch.android.facerecognition;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ImageFormat;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.YuvImage;
+import android.media.Image;
+import android.util.Pair;
+
+import com.google.mlkit.vision.common.InputImage;
+
+import org.tensorflow.lite.Interpreter;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.ReadOnlyBufferException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ImageUtils {
+
+    public static boolean flipX = false;
+
+    private static final float IMAGE_MEAN = 128.0f;
+    private static final float IMAGE_STD = 128.0f;
+    private static final int INPUT_SIZE = 112;
+    private static final int OUTPUT_SIZE=192;
+
+    /** Bitmap Converter */
+    public static Bitmap croppedFace(Bitmap bmp, int rotation, Rect boundingBox) {
+        //Convert media image to Bitmap
+        Bitmap frame_bmp = bmp;
+
+        //Adjust orientation of Face
+        Bitmap frame_bmp1 = rotateBitmap(frame_bmp, rotation, flipX);
+
+        //Crop out bounding box from whole Bitmap(image)
+        float padding = 0.0f;
+        RectF adjustedBoundingBox = new RectF(
+                boundingBox.left - padding,
+                boundingBox.top - padding,
+                boundingBox.right + padding,
+                boundingBox.bottom + padding);
+        Bitmap cropped_face = getCropBitmapByCPU(frame_bmp1, adjustedBoundingBox);
+
+        // Resize bitmap to 112,112
+        return getResizedBitmap(cropped_face);
+    }
+
+    public static String recognizeImage(final Bitmap bitmap, Interpreter tfLite, HashMap<String, SimilarityClassifier.Recognition> registered) {
+        // Create ByteBuffer to store normalized image
+        ByteBuffer imgData = ByteBuffer.allocateDirect(INPUT_SIZE * INPUT_SIZE * 3 * 4);
+
+        imgData.order(ByteOrder.nativeOrder());
+
+        int[] intValues = new int[INPUT_SIZE * INPUT_SIZE];
+
+        //get pixel values from Bitmap to normalize
+        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        imgData.rewind();
+
+        for (int i = 0; i < INPUT_SIZE; ++i) {
+            for (int j = 0; j < INPUT_SIZE; ++j) {
+                int pixelValue = intValues[i * INPUT_SIZE + j];
+                imgData.putFloat((((pixelValue >> 16) & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+                imgData.putFloat((((pixelValue >> 8) & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+                imgData.putFloat(((pixelValue & 0xFF) - IMAGE_MEAN) / IMAGE_STD);
+            }
+        }
+        //imgData is input to our model
+        Object[] inputArray = {imgData};
+
+        Map<Integer, Object> outputMap = new HashMap<>();
+
+        float[][] embeddings = new float[1][OUTPUT_SIZE]; //output of model will be stored in this variable
+
+        outputMap.put(0, embeddings);
+
+        tfLite.runForMultipleInputsOutputs(inputArray, outputMap); //Run model
+
+        float distance;
+
+        //Compare new face with saved Faces.
+        String name = null;
+
+        if (registered.size() > 0) {
+
+            final Pair<String, Float> nearest = findNearest(embeddings[0], registered);//Find closest matching face
+
+            if (nearest != null) {
+
+                distance = nearest.second;
+                if(distance<1.000f) //If distance between Closest found face is more than 1.000 ,then output UNKNOWN face.
+                    name = nearest.first;
+            }
+        }
+
+        if (name == null) {
+            name = "face" + registered.size();
+
+            SimilarityClassifier.Recognition result = new SimilarityClassifier.Recognition("0", "1", -1f);
+            result.setExtra(embeddings);
+
+            registered.put(name, result);
+        }
+
+        return name;
+    }
+
+    //Compare Faces by distance between face embeddings
+    private static Pair<String, Float> findNearest(float[] emb, HashMap<String, SimilarityClassifier.Recognition> registered) {
+
+        Pair<String, Float> ret = null;
+        for (Map.Entry<String, SimilarityClassifier.Recognition> entry : registered.entrySet()) {
+
+            final String name = entry.getKey();
+            final float[] knownEmb = ((float[][]) entry.getValue().getExtra())[0];
+
+            float distance = 0;
+            for (int i = 0; i < emb.length; i++) {
+                float diff = emb[i] - knownEmb[i];
+                distance += diff*diff;
+            }
+            distance = (float) Math.sqrt(distance);
+            if (ret == null || distance < ret.second) {
+                ret = new Pair<>(name, distance);
+            }
+        }
+
+        return ret;
+    }
+
+    public static Bitmap rotateBitmap(Bitmap bitmap, int rotationDegrees, boolean flipX) {
+        Matrix matrix = new Matrix();
+
+        // Rotate the image back to straight.
+        matrix.postRotate(rotationDegrees);
+
+        // Mirror the image along the X or Y axis.
+        matrix.postScale(flipX ? -1.0f : 1.0f, 1.0f);
+        Bitmap rotatedBitmap =
+                Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+
+        // Recycle the old bitmap if it has changed.
+        if (rotatedBitmap != bitmap) {
+            bitmap.recycle();
+        }
+        return rotatedBitmap;
+    }
+
+    public static Bitmap getCropBitmapByCPU(Bitmap source, RectF cropRectF) {
+        Bitmap resultBitmap = Bitmap.createBitmap(
+            (int) cropRectF.width(), (int) cropRectF.height(), Bitmap.Config.ARGB_8888
+        );
+        Canvas canvas = new Canvas(resultBitmap);
+
+        // draw background
+        Paint paint = new Paint(Paint.FILTER_BITMAP_FLAG);
+        paint.setColor(Color.WHITE);
+        canvas.drawRect(//from  w w  w. ja v  a  2s. c  om
+                new RectF(0, 0, cropRectF.width(), cropRectF.height()),
+                paint);
+
+        Matrix matrix = new Matrix();
+        matrix.postTranslate(-cropRectF.left, -cropRectF.top);
+
+        canvas.drawBitmap(source, matrix, paint);
+
+        if (source != null && !source.isRecycled()) {
+            source.recycle();
+        }
+
+        return resultBitmap;
+    }
+
+    public static Bitmap getResizedBitmap(Bitmap bm) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) 112) / width;
+        float scaleHeight = ((float) 112) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+        bm.recycle();
+        return resizedBitmap;
+    }
+
+}
---
+++ app/src/main/java/com/irhammuch/android/facerecognition/MainActivityImage.java
@@ -0,0 +1,177 @@
+package com.irhammuch.android.facerecognition;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
+
+import org.tensorflow.lite.Interpreter;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+public class MainActivityImage extends AppCompatActivity {
+
+    private static final Logger logger = Logger.getLogger(MainActivity.class.getName());
+
+    private static final String TAG = "MainActivityImage";
+
+    // UI
+    private ProgressDialog mDialog;
+    private Toolbar mToolbar;
+    private FloatingActionButton mFabActionBt;
+
+    private Interpreter tfLite;
+
+    private FaceDetector faceDetector;
+
+    private final HashMap<String, SimilarityClassifier.Recognition> registered = new HashMap<>(); //saved Faces
+
+    private final HashMap<String, InputImage> inputImages = new HashMap<>();
+
+    private final HashMap<String, List<String>> imageTags = new HashMap<>(); // image tags.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_image);
+        setSupportActionBar(mToolbar);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(getString(R.string.app_name));
+
+        loadModel();
+
+        faceDetector = FaceDetection.getClient(new FaceDetectorOptions.Builder()
+                .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+                .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+                .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+                .build()
+        );
+
+        setupUI();
+    }
+
+    protected void setupUI() {
+        mFabActionBt = (FloatingActionButton) findViewById(R.id.start);
+        mFabActionBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivityImage.this, "Start", Toast.LENGTH_SHORT).show();
+                try {
+                    for (String f: getAssets().list("face_images/")) {
+                        logger.info("Processing face_images/" + f);
+                        analyze("face_images/" + f);
+                    }
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private Boolean analyze(@NonNull String image_path) throws InterruptedException {
+        InputImage image = null;
+        try {
+            image = inputImages.get(image_path);
+            if (image == null) {
+                image = InputImage.fromBitmap(BitmapFactory.decodeStream(getAssets().open(image_path)), 0);
+                inputImages.put(image_path, image);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assert image != null;
+
+        return faceDetector.process(image)
+                .addOnSuccessListener(faces -> onSuccessListener(faces, image_path))
+                .addOnFailureListener(e -> e.printStackTrace())
+                .addOnCompleteListener(task -> logger.info("Done of " + image_path)).isComplete();
+    }
+
+    private void onSuccessListener(List<Face> faces, String image_path) {
+        logger.info("Find " + faces.size() + " faces.");
+
+        for (Face face: faces) {
+            try {
+                Rect boundingBox = face.getBoundingBox();
+
+                Bitmap bitmap = ImageUtils.croppedFace(
+                        BitmapFactory.decodeStream(getAssets().open(image_path)),
+                        inputImages.get(image_path).getRotationDegrees(),
+                        boundingBox
+                );
+
+                String name = ImageUtils.recognizeImage(bitmap, tfLite, registered);
+
+                List<String> tags = imageTags.getOrDefault(image_path, new ArrayList<>());
+                tags.add(name);
+                imageTags.put(image_path, tags);
+
+                System.out.println("Image Tags: " + imageTags.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void showDiaglog(String title) {
+        dismissDialog();
+        mDialog = ProgressDialog.show(MainActivityImage.this, title, "process..", true);
+    }
+
+    private void dismissDialog() {
+        if (mDialog != null) {
+            mDialog.dismiss();
+            mDialog = null;
+        }
+    }
+
+    /** Model loader */
+    @SuppressWarnings("deprecation")
+    private void loadModel() {
+        try {
+            //model name
+            String modelFile = "mobile_face_net.tflite";
+            tfLite = new Interpreter(loadModelFile(MainActivityImage.this, modelFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private MappedByteBuffer loadModelFile(Activity activity, String MODEL_FILE) throws IOException {
+        AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(MODEL_FILE);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+}
